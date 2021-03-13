c2c76194048cab2b6847e452c052fc1058e3d465
Myklon <livroklon@gmail.com>
2021-03-13 22:36:04 +0200

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    bracketsConfig = bracketsConfig.map(element => element.join(''));
+    let check = true;
+    while (check) {
+      check = false;
+      for (let i = 0; i < bracketsConfig.length; i++ ) 
+        if (str.includes(bracketsConfig[i])) {
+            check = true;
+            str = str.replace(bracketsConfig[i], '')
+        }  
+    }
+    if (str == '') return true;
+    else return false;
 }
